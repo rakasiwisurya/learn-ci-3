9f0c48f71a6ebec837e83dde810e4cdd7ee625d7
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2024-09-05 15:54:04 +0700

    done looping

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -7,7 +7,13 @@ class Blog extends CI_Controller
     {
         $data['title'] = 'My First Blog';
         $data['heading'] = 'My First Blog';
-        $data['message'] = 'This is my first blog which build using Codeigniter 3 Framework.';
+        $data['messages'] = [
+            'This is my first blog which build using Codeigniter 3 Framework.',
+            'This is my second blog which build using Codeigniter 3 Framework.',
+            'This is my third blog which build using Codeigniter 3 Framework.',
+            'This is my fourth blog which build using Codeigniter 3 Framework.',
+            'This is my fifth blog which build using Codeigniter 3 Framework.',
+        ];
 
         $this->load->view('templates/header', $data);
         $this->load->view('blog', $data);
--- application/views/blog.php
+++ application/views/blog.php
@@ -1,5 +1,11 @@
 <h1><?php echo $heading ?></h1>
 
 <div id="body">
-    <p><?php echo $message ?></p>
+    <!-- <?php foreach ($messages as $message) { ?>
+        <p><?php echo $message ?></p>
+    <?php } ?> -->
+
+    <?php foreach ($messages as $message) : ?>
+        <p><?php echo $message ?></p>
+    <?php endforeach; ?>
 </div>
